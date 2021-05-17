40259f7b57901b8dff7f4cc2ccb50a47b3486071
Antwan Grant <twanjobs68@gmail.com>
2021-05-17 19:22:31 -0400

    first commit

---
+++ src/Component/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            < div className="homepg">
+               
+                <div className="tgflixbox">
+                    <h1>TGFLIX</h1>
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
